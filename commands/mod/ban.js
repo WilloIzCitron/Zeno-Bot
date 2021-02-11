67607c5904a2a2b59acf79a7394fc2162f9fb451
null <60427892+vierofernando@users.noreply.github.com>
2021-02-11 12:01:27 +0700

    Update ban.js

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -1,11 +1,10 @@
 exports.run = async (client, message, args) => {
-  if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('you missing Ban Members perm');
+  if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('you\'re missing the `ban members` permission.');
   const user = message.mentions.members.first() || message.guild.members.cache.get(message.content.split(" ").slice(1).join(" "));
-  if(!user)return message.channel.send('you missing mention/id');
-  if(user.hasPermission('ADMINISTRATOR') || user.roles.highest.hoist === true)return message.channel.send("you cant ban admin or highest role")
-  if(user === message.author.id)return message.channel.send("you cant ban yourself")
-  reason = message.content.split(" ").slice(2).join(" ")
-  if(!reason)return reason = "no reason provided"
+  if(!user)return message.channel.send('you are missing at least a mention/id');
+  if(user.hasPermission('ADMINISTRATOR') || user.roles.highest.hoist === true) return message.channel.send("you can't ban an admin or highest role")
+  if(user === message.author.id) return message.channel.send("you can't ban yourself nerd")
+  const reason = message.content.split(" ").slice(2).join(" ") || "no reason provided";
   await user.ban({ days: 0, reason: reason })
   message.channel.send(`Successfully ${user.user.tag} is banned with reason: ${reason}`)
   user.send(`You have been banned from ${message.guild.name}:\nModerator: ${message.author.tag}\nReason: ${reason}`)
