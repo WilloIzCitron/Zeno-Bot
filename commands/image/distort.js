c608a61aaf8f80b762179802e184d9e9d29f30df
null <60427892+vierofernando@users.noreply.github.com>
2021-02-11 11:39:02 +0700

    shorten distort.js

--- commands/image/distort.js
+++ commands/image/distort.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js")
 
 exports.run = async (client, message, args) => {
-  user = message.mentions.users.first() || message.author;
-  api = `http://api.moe.alvindaldi.my.id/api/distort?image=${user.avatarURL({ size: 2048, format: 'png'})}`
-  const attachment = new Discord.MessageAttachment(api, "distort.png");
+  const user = message.mentions.users.first() || message.author;
+  const attachment = new Discord.MessageAttachment(`http://api.moe.alvindaldi.my.id/api/distort?image=${user.avatarURL({ size: 2048, format: 'png'})}`, "distort.png");
   return message.channel.send(attachment);
   
 }
