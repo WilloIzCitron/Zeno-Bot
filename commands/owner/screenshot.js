2cb67dcab7c9d74834d8ff99acfe8dc791009554
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2022-05-09 12:12:27 +0700

    yay

--- commands/owner/screenshot.js
+++ commands/owner/screenshot.js
@@ -20,8 +20,8 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
     let url = message.content.split(" ").slice(1).join(" ");
     if (!url) return message.channel.send("Please provide a url!");
-    const buffer = new MessageAttachment(`https://shot.screenshotapi.net/screenshot?&url=${encodeURIComponent(url)}&width=1920&height=1080&output=image`, "screenshot.png");
-    message.channel.send({ files: [buffer] });
+    const buffer = `https://shot.screenshotapi.net/screenshot?&url=${encodeURIComponent(url)}&width=1920&height=1080&output=image&file_type=png`;
+    message.channel.send({ files: [{ attachment: buffer, name: 'screenshot.png' }] });
 };
 
 exports.help = {
