db4c8cbea2d42011ec6948f059d74852186fb1b5
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2022-05-09 10:40:43 +0700

    screenshot but still brainstorming

---
+++ commands/util/screenshot.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+const Discord = require('discord.js');
+
+const getScreenshot = async (url) => {
+    const browser = await puppeteer.launch({
+        defaultViewport: {
+            width:1920,
+            height:1080
+      }});
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForTimeout(3000)
+    const buffer = await page.screenshot({type: 'png'});
+    await browser.close();
+
+    return buffer;
+}
+
+exports.run = async (client, message, args) => {
+    let url = message.content.split(" ").slice(1).join(" ");
+    if (!url) return message.channel.send("Please provide a url!");
+    const buffer = await getScreenshot(url);
+    message.channel.send({ files: [buffer] });
+};
+
+exports.help = {
+    name: "screenshot",
+    description: "get website screenshot from url",
+    usage: "screenshot <url>",
+    example: "screenshot https://google.com",
+    api: ""
+  }
+  
+  exports.conf = {
+    aliases: ["ss"],
+    cooldown: 7
+  }
