a4f31fcef18a5556ac1b63933e65c4c91673a70b
WilloIzCitron <tegasaziz897@gmail.com>
2022-07-03 21:28:39 +0700

    update

--- commands/util/say.js
+++ commands/util/say.js
@@ -3,14 +3,19 @@ exports.run = async (client, message, args) => {
   const text = message.content.split(" ").slice(1).join(" ") || "please add a text";
   if (text.includes("--reply")) {
     return message.reply(text.replace(/\-\-reply/g, ""))
-  }
+  } else if (text.includes("--dm")) {
+    return message.author.send(text.replace(/\-\-dm/g, ""))
+  } else if (text.includes("--silent")) {
+    message.delete()
+    message.channel.send(text.replace(/\-\-silent/g, ""))
+  } else
   return message.channel.send(text);
 }
 
 exports.help = {
   name: "say",
-  description: "send a text command",
-  usage: "say <arg> or say  <arg> --reply",
+  description: "send a text command there is a parameter --reply to reply to the message, --dm to dm the user, --silent to send the message silently",
+  usage: "say <arg> or say <arg> [parameters]",
   example: "say hey",
   api: ""
 }
